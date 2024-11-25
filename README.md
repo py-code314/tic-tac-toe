cd568a6ad887ea087a4079ce592d7d86601f3a61
Srikanth K <122169588+py-code314@users.noreply.github.com>
2024-11-25 08:36:22 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tic Tac Toe game for The Odin Project curriculum
---
+++ main.js
@@ -0,0 +1,7 @@
+// Test code below.
+// const text = document.querySelector('.text');
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', () => {
+//   text.textContent = 'Hello';
+// })
